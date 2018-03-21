d110d3a5d7e06d9be7fa9d6a0255ffd373cf6b9d
lyin <ling95613@163.com>
2018-03-21 13:33:02 +0800

    修改数据库地址

--- src/model/index.js
+++ src/model/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const DB_URL = 'mongodb://127.0.0.1:27017/qidian';
+const DB_URL = 'mongodb://60.205.179.15:27017/qidian';
 
 //连接数据库
 mongoose.connect(DB_URL, function (err, db) {
